2c8c4d44d130a0b2ce7fc371af3f4a1efefdefe6
shabdagrawaldiary-prog <shabdagrawaldiary@gmail.com>
2026-08-31 21:55:45 +0530

    Add SubstringCompare Solution

---
+++ Submission of Array and String Operations/String/SubstringCompare.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class SubstringCompare {
+
+    public static String createSubstring(String str, int start, int end) {
+        String result = "";
+
+        for (int i = start; i < end; i++) {
+            result = result + str.charAt(i);
+        }
+
+        return result;
+    }
+
+    public static boolean compareStrings(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.charAt(i) != str2.charAt(i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.next();
+
+        System.out.print("Enter start index: ");
+        int start = sc.nextInt();
+
+        System.out.print("Enter end index: ");
+        int end = sc.nextInt();
+
+        String substring1 = createSubstring(str, start, end);
+        String substring2 = str.substring(start, end);
+
+        System.out.println("Substring using charAt(): " + substring1);
+        System.out.println("Substring using substring(): " + substring2);
+
+        boolean result = compareStrings(substring1, substring2);
+
+        System.out.println("Both substrings are equal: " + result);
+    }
+}
+
