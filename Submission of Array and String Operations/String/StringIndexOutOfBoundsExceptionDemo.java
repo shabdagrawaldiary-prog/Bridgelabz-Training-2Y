b779e2d526491aa061e8f1dfea880a44beb7cdd6
shabdagrawaldiary-prog <shabdagrawaldiary@gmail.com>
2026-08-31 22:05:31 +0530

    Add StringIndexOutOfBoundsExceptionDemo Solution

---
+++ Submission of Array and String Operations/String/StringIndexOutOfBoundsExceptionDemo.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class StringIndexOutOfBoundsExceptionDemo {
+
+    public static void generateException(String text) {
+        System.out.println(text.charAt(text.length() + 1));
+    }
+
+    public static void handleException(String text) {
+        try {
+            System.out.println(text.charAt(text.length() + 1));
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("StringIndexOutOfBoundsException handled.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String text = sc.next();
+
+        generateException(text);
+
+        handleException(text);
+    }
+}
+
